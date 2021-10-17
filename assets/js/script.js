153ac1e64204bd559352eafc5d7e550ddd6b8228
Caroline Pastorelli <caroline.pastorelli@gmail.com>
2021-10-16 19:11:50 -0700

    created outline of rows for dayplanner and set function up to correctly display times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const saveBtn = document.querySelector('saveBtn');
 const currentDay = document.querySelector('#currentDay');
 const greeting = document.querySelector('.greeting');
-const numOfBlocks = 12;
+const numOfBlocks = 24;
 const time = "H mm A";
-var amPM = "";
+// var amPM = "";
+var counter = 0;
+
+
 
 //make functionshow current day with moment js
 function todaysDate() {
@@ -21,32 +24,104 @@ function getCurrentTime (getTime) {
     // grabs hour (24 hour format), minute (00-59), and AM/PM 
     var rightNow = moment().format(getTime);
 
-    console.log(rightNow);
-
     // splits time into array of three parts [0]Hour, [1]Min, [2]Am/Pm
     rightNow = rightNow.split(" ");
 
-    console.log(rightNow);
-
 // if index[2] is AM, show good morning, else show good evening
-if(rightNow[rightNow.length - 1] == "AM"){
-    
-    console.log("Good Morning!");
+if(rightNow[2] === "AM"){
 
-    greeting.innerHTML = "Good Morning!";
+    // Show Good Morning on screen
+    greeting.innerHTML = "Good Morning!";   
 } else {
-    console.log("Good Afternoon!");
 
+    // Show Good Afternoon on screen
     greeting.innerHTML = "Good Afternoon!";
 }
 
-
+return rightNow;
 }
+
+// function incHour(thisHour){
+// if(thisHour > 12){
+//     thisHour -= 12;
+//     console.log("past 12")
+//     //I feel like I need to add AM/PM HEre
+//     }
+// // else if(thisHour === 12){
+// //     thisHour = 1;
+// // //? meh..
+// // }
+// // else{
+// //     thisHour++;
+// // }
+
+// }
+
+
+function incHour() {
+
+    var printHour;
+
+    if(counter === 0) {
+        printHour = 12 + "AM";
+        counter++;
+    }else if(counter != 0 && counter < 12){
+        printHour = counter + "AM";
+        counter++;
+    } else if(counter >= 12 && counter <= 24){
+        if (counter === 12){
+            printHour = counter + "PM";
+            counter++;
+        }else{
+            printHour = counter - 12;
+            printHour += "PM";
+            counter++;
+        }
+
+    }
+
+    return printHour;
+}
+
 
 
 // Function to make the blocks of time
 function createTimeBlock(timeblock) {
-getCurrentTime(time);
+
+var timeArr = getCurrentTime(time);
+
+//var theHour = Number(timeArr[0]);
+
+//var theMin = Number(timeArr[1]);
+
+
+for(var i = 0; i < timeblock; i++){
+
+    //var timeOutput = theHour + timeArr[2]
+    var showHour = incHour();
+
+    console.log("timeOutput is :" + showHour);
+    
+
+    //make a new div block and assign the class row to it
+    $newTimeBlock = $("<div>").addClass("row"); 
+
+    //take the time from array[0] and set to a new variable
+    $timeText = $("<p>").text(showHour);
+
+    $newCol = $("<div>").addClass("col-2 align-middle").append($timeText);
+
+    $newTextArea = $("<textarea>").addClass("col-8").text("");
+
+    $newSaveBtn = $("<div>").addClass("saveBtn col-1");
+
+    $newTimeBlock.append($newCol, $newTextArea, $newSaveBtn);
+    $(".container").append($newTimeBlock)
+
+    
+    
+
+    }
 
 
 }
